91064601485694b3dac9674a08108d78930a167a
Kaveesha2000 <kaveesha200066@gmail.com>
2022-07-07 12:58:12 +0530

    adding application.properties file

--- src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,7 +1,10 @@
 package lk.ijse.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -12,46 +15,53 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.naming.NamingException;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "lk.ijse.spring.repo")
 @EnableTransactionManagement
+@PropertySource("classpath:application.properties")
 public class JPAConfig {
 
+    @Autowired
+    Environment env;
+
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va) {
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
-        bean.setJpaVendorAdapter(va); // Vendor (Hibernate)
-        bean.setDataSource(ds); //Connection
-        bean.setPackagesToScan("lk.ijse.spring.entity"); // location of the entity
+        bean.setJpaVendorAdapter(va);
+        bean.setDataSource(ds);
+        bean.setPackagesToScan(env.getRequiredProperty("entity.package.name"));
         return bean;
     }
 
     @Bean
-    public DataSource dataSource(){
+    public DataSource dataSource() throws NamingException {
         DriverManagerDataSource dataSource= new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306/CarRentalSystem?createDatabaseIfNotExist=true");
-        dataSource.setUsername("root");
-        dataSource.setPassword("1234");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(env.getRequiredProperty("my.app.url"));
+        dataSource.setUsername(env.getRequiredProperty("my.app.username"));
+        dataSource.setPassword(env.getRequiredProperty("my.app.password"));
+        dataSource.setDriverClassName(env.getRequiredProperty("my.app.driverclassname"));
         return dataSource;
+
+        //return (DataSource) new JndiTemplate().lookup("java:comp/env/jdbc/pool");
     }
 
     @Bean
-    public JpaVendorAdapter jpaVendorAdapter(){
+    public JpaVendorAdapter jpaVendorAdapter() {
         HibernateJpaVendorAdapter vendor = new HibernateJpaVendorAdapter();
-        vendor.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        vendor.setDatabasePlatform(env.getRequiredProperty("my.app.dialect"));
         vendor.setDatabase(Database.MYSQL);
         vendor.setShowSql(true);
         vendor.setGenerateDdl(true);
         return vendor;
     }
 
-    //managing the transaction
     @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
         return new JpaTransactionManager(emf);
     }
+
 }
