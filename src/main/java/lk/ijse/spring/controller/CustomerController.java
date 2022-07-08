622dd9dd6c89c93711d70cdcf80b34cc16c9a24a
Kaveesha2000 <kaveesha200066@gmail.com>
2022-07-08 11:04:38 +0530

    creating the CustomerController

--- src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,4 +1,42 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/customer")
+@CrossOrigin
 public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCustomer(@ModelAttribute CustomerDTO customerDTO){
+        customerService.saveCustomer(customerDTO);
+        return new ResponseUtil(200,"Save",null);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO customerDTO){
+        customerService.updateCustomer(customerDTO);
+        return new ResponseUtil(200,"Updated",null);
+    }
+
+    @DeleteMapping(params = {"id"},produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCustomer(@RequestParam String id){
+        customerService.deleteCustomer(id);
+        return new ResponseUtil(200,"Deleted",null);
+    }
+
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCustomer(@PathVariable String id){
+        return new ResponseUtil(200,"Ok",customerService.searchCustomer(id));
+    }
 }
