b5459008f44124a5a6594b77f74816f78522b332
Kaveesha2000 <kaveesha200066@gmail.com>
2022-08-03 09:59:01 +0530

    navigation done

---
+++ FrontEnd/web/controller/DashboardController.js
@@ -0,0 +1,95 @@
+$("#dashboardDriverBtn").click(function () {
+    $("#homePageSection").css('display', 'none');
+    $("#logInSection").css('display', 'none');
+    $("#dashboardSection").css('display', 'none');
+    $("#adminSection").css('display', 'none');
+    $("#carSection").css('display', 'none');
+    $("#customerSection").css('display', 'none');
+    $("#customerViewSection").css('display', 'none');
+    $("#rentSection").css('display', 'none');
+    $("#reservationSection").css('display', 'none');
+    $("#reserveViewSection").css('display', 'none');
+    $("#scheduleSection").css('display', 'none');
+    $("#incomeSection").css('display', 'none');
+    $("#driverSection").css('display', 'block');
+})
+
+$("#dashboardCarBtn").click(function () {
+    $("#homePageSection").css('display', 'none');
+    $("#logInSection").css('display', 'none');
+    $("#dashboardSection").css('display', 'none');
+    $("#adminSection").css('display', 'none');
+    $("#carSection").css('display', 'block');
+    $("#customerSection").css('display', 'none');
+    $("#customerViewSection").css('display', 'none');
+    $("#rentSection").css('display', 'none');
+    $("#reservationSection").css('display', 'none');
+    $("#reserveViewSection").css('display', 'none');
+    $("#scheduleSection").css('display', 'none');
+    $("#incomeSection").css('display', 'none');
+    $("#driverSection").css('display', 'none');
+})
+
+$("#dashboardScheduleBtn").click(function () {
+    $("#homePageSection").css('display', 'none');
+    $("#logInSection").css('display', 'none');
+    $("#dashboardSection").css('display', 'none');
+    $("#adminSection").css('display', 'none');
+    $("#carSection").css('display', 'none');
+    $("#customerSection").css('display', 'none');
+    $("#customerViewSection").css('display', 'none');
+    $("#rentSection").css('display', 'none');
+    $("#reservationSection").css('display', 'none');
+    $("#reserveViewSection").css('display', 'none');
+    $("#scheduleSection").css('display', 'block');
+    $("#incomeSection").css('display', 'none');
+    $("#driverSection").css('display', 'none');
+})
+
+$("#dashboardIncomeBtn").click(function () {
+    $("#homePageSection").css('display', 'none');
+    $("#logInSection").css('display', 'none');
+    $("#dashboardSection").css('display', 'none');
+    $("#adminSection").css('display', 'none');
+    $("#carSection").css('display', 'none');
+    $("#customerSection").css('display', 'none');
+    $("#customerViewSection").css('display', 'none');
+    $("#rentSection").css('display', 'none');
+    $("#reservationSection").css('display', 'none');
+    $("#reserveViewSection").css('display', 'none');
+    $("#scheduleSection").css('display', 'none');
+    $("#incomeSection").css('display', 'block');
+    $("#driverSection").css('display', 'none');
+})
+
+$("#dashboardReservationViewBtn").click(function () {
+    $("#homePageSection").css('display', 'none');
+    $("#logInSection").css('display', 'none');
+    $("#dashboardSection").css('display', 'none');
+    $("#adminSection").css('display', 'none');
+    $("#carSection").css('display', 'none');
+    $("#customerSection").css('display', 'none');
+    $("#customerViewSection").css('display', 'none');
+    $("#rentSection").css('display', 'none');
+    $("#reservationSection").css('display', 'none');
+    $("#reserveViewSection").css('display', 'block');
+    $("#scheduleSection").css('display', 'none');
+    $("#incomeSection").css('display', 'none');
+    $("#driverSection").css('display', 'none');
+})
+
+$("#dashboardCustomerViewBtn").click(function () {
+    $("#homePageSection").css('display', 'none');
+    $("#logInSection").css('display', 'none');
+    $("#dashboardSection").css('display', 'none');
+    $("#adminSection").css('display', 'none');
+    $("#carSection").css('display', 'none');
+    $("#customerSection").css('display', 'none');
+    $("#customerViewSection").css('display', 'block');
+    $("#rentSection").css('display', 'none');
+    $("#reservationSection").css('display', 'none');
+    $("#reserveViewSection").css('display', 'none');
+    $("#scheduleSection").css('display', 'none');
+    $("#incomeSection").css('display', 'none');
+    $("#driverSection").css('display', 'none');
+})
